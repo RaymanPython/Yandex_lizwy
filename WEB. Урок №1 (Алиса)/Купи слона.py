bc18babdf4296a9975c0b4c459f5bc8f1d893aa6
Ray <ggammi1007@yandex.ru>
2022-05-02 22:52:22 +0300

    Gelolw
    Signed-off-by: Ray <ggammi1007@yandex.ru>

---
+++ WEB. Урок №1 (Алиса)/Купи слона.py
@@ -0,0 +1,47 @@
+from flask import Flask, request
+import logging
+import json
+
+app = Flask(__name__)
+logging.basicConfig(level=logging.INFO)
+sessionStorage = {}
+
+
+@app.route('/post', methods=['POST'])
+def main():
+    logging.info(f'Request: {request.json!r}')
+    response = {
+        'session': request.json['session'],
+        'version': request.json['version'],
+        'response': {
+            'end_session': False
+        }
+    }
+
+    handle_dialog(request.json, response)
+    logging.info(f'Response:  {response!r}')
+    return json.dumps(response)
+
+
+def handle_dialog(req, res):
+    user_id = req['session']['user_id']
+    if req['session']['new']:
+        sessionStorage[user_id] = {
+            'suggests': [
+                "Не хочу.",
+                "Не буду.",
+                "Отстань!",
+            ]
+        }
+        res['response']['text'] = 'Привет! Купи слона!'
+        res['response']['buttons'] = get_suggests(user_id)
+        return
+    if req['request']['original_utterance'].lower() in [
+        'ладно',
+        'куплю',
+        'покупаю',
+        'хорошо'
+    ]:
+        res['response']['text'] = 'Слона можно найти на Яндекс.Маркете!'
+        res['response']['end_session'] = True
+        return
